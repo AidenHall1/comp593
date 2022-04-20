43f4c00270fa922c73c7d095b2cb8f1763867bf5
AidenHall1 <98234090+AidenHall1@users.noreply.github.com>
2022-04-19 21:52:46 -0700

    lab 9
    Lab 9, the images and the main cod eand other file that i imported the functions,

--- pokeapi.py
+++ pokeapi.py
@@ -1,4 +1,6 @@
+
 import requests
+
 
 #Gets poke info
 #pram name = Pokemon name
@@ -17,4 +19,29 @@ def get_pokemon_info(name):
     else:
         print("Did not work", resp_message.status_code)
         return
-        
+#returns the list of the pokemon names
+def get_list(limit=100, offset=0):
+    url = 'https://pokeapi.co/api/v2/pokemon'
+
+    params = {
+        'limit' : limit,
+        'offset' : offset
+    }
+
+    resp_messag = requests.get(url, params=params)
+
+    if resp_messag.status_code == 200:
+
+        dict = resp_messag.json()
+
+        return [p['name']for p in dict['results']]
+
+    else: 
+       
+        print("Did not work", resp_messag.status_code)
+        return
+#dictonry to grab the url for the photos
+def get_poke_image_url(name):
+    poke_dict = get_pokemon_info(name)
+    if poke_dict:
+        return poke_dict['sprites']['other']['official-artwork']['front_default']
